19aee089e306fd6de4faae24dc067916f5afccc3
Raphael Brandão <raphael@mixmax.com>
2022-03-23 20:49:04 -0300

    change the text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          You already know GitHub
+          You already know GitHub Learn Git
         </a>
       </header>
     </div>
