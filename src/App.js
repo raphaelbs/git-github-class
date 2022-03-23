45568de296b16074ab50bd78be61025273d1e765
Gabriel Rodrigues <84934507+gabezrodz@users.noreply.github.com>
2022-03-23 20:04:57 -0300

    Apply suggestions from code review
    Co-authored-by: Raphael Brandão <raphael.b.souza@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GitHub
+          You already know GitHub
         </a>
       </header>
     </div>
